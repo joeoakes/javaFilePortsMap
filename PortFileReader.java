a81f80445e19067397ccd45036ae95f0b5b379fb
joeoakes <jxo19@psu.edu>
2023-11-08 21:03:39 -0500

    commit

---
+++ PortFileReader.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PortFileReader {
+    public static void main(String[] args) {
+        String filename = "ports.txt"; // Replace with the path to your file
+        Map<String, Integer> portMap = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Split the line into key-value pairs (e.g., "http=80")
+                String[] parts = line.split("=");
+
+                if (parts.length == 2) {
+                    String serviceName = parts[0].trim();
+                    int portNumber = Integer.parseInt(parts[1].trim());
+
+                    // Put the key-value pair into the map
+                    portMap.put(serviceName, portNumber);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Print the contents of the map
+        for (Map.Entry<String, Integer> entry : portMap.entrySet()) {
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+    }
+}
